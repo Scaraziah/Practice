72afa33db10dc2211f2833a2f5a02d7e289c3a62
Larry Hatcer <actswolf@gmail.com>
2022-09-08 19:02:26 -0500

    Fixed to where it is on the same line. Chart will not put it on the same line.

--- ProblemSolvingPart1/probSolvP1.jsx
+++ ProblemSolvingPart1/probSolvP1.jsx
@@ -4,12 +4,21 @@
 //     a. Write code that takes a string as input and returns the string reversed
 //     b. i.e. “Hello” will be returned as “olleH”
 
-function revString(){
-    for(let i= revArr.length -1 ; i >= 0; i--){
-        let newArr = revArr.charAt(i);
-        let myWord = newArr;
-        console.log(myWord);
-    }
-}
+// function revString(){
+//     for(let i= revArr.length -1 ; i >= 0; i--){
+//         let newArr = revArr.charAt(i);
+//         let myWord = newArr;
+//         console.log(myWord);
+//     }
+// }
 let revArr = prompt("Enter word in reverse.")
-revString(revArr)
+// revString(revArr)
+
+let joinArray;
+function reverseString(str) {
+    var splitString = str.split(""); 
+    var reverseArray = splitString.reverse(); 
+    var joinArray = reverseArray.join("");
+    return joinArray; 
+}
+console.log(reverseString(revArr))
