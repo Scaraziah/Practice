c6b4937cdf92724e05dd58eedef2d0bc8e29c103
Larry Hatcer <actswolf@gmail.com>
2022-09-15 12:45:04 -0500

    Finished 3rd problem. Added more explination on the previous problem to understand.

--- ProblemSolvingPart1/probSolvP1.jsx
+++ ProblemSolvingPart1/probSolvP1.jsx
@@ -26,9 +26,40 @@ console.log(reverseString(revArr))
 // 2. Capitalize letter
 //   a. Write code that takes a string as input and capitalize the first letter of each word. Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World”
 
+// The / Is the beginning of a regex (regular expression). 
+// The \w looks for all word characters in a string. A word character is a character from a-z, A-Z, 0-9, including the _ (underscore) character. 
+// Now the \S is any character that is NOT a whitespace character. You were confusing it with \s(lowercase) which is for whitespace characters. 
+// The * is a character that says to look for n zero or more times, where n is the metacharacter(the backslash tokens in the regex) in the regex.
+
+// If you need more help go to: http://www.w3schools.com/jsref/jsref_obj_regexp.asp
+
 function toTitleCase(str) {
     return str.replace(/\w\S*/g, function(txt){
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
 }
 console.log(toTitleCase("hello world"));
+
+
+// 3. Compress a string of characters
+//     a. For example, an input of "aaabbbbbccccaacccbbbaaabbbaaa" would compress to "3a5b4c2a3c3b3a3b3a"
+
+let rLongNum = "aaabbbbbccccaacccbbbaaabbbaaa";
+
+function compNumb(str){
+    let result = "";
+    let count = 0;
+    for(let i = 0; i < str.length; i++){
+        count++
+        if(str[i] != str[i + 1]){
+            result = result + str[i] + count;
+            count = 0;
+        }
+    }
+    console.log(result)
+}
+
+compNumb(rLongNum)
+// let chartNumb = compressNumb.join();
+// console.log("Compressed Word ", chartNumb)
+
