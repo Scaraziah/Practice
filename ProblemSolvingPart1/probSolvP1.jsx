3c0679e6984b16df68d42900d06ce4416934522f
Larry Hatcer <actswolf@gmail.com>
2022-09-15 11:26:05 -0500

    Complete problem 2. Looking into it as I don't understand some of the code.

--- ProblemSolvingPart1/probSolvP1.jsx
+++ ProblemSolvingPart1/probSolvP1.jsx
@@ -22,3 +22,13 @@ function reverseString(str) {
     return joinArray; 
 }
 console.log(reverseString(revArr))
+
+// 2. Capitalize letter
+//   a. Write code that takes a string as input and capitalize the first letter of each word. Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World”
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+console.log(toTitleCase("hello world"));
