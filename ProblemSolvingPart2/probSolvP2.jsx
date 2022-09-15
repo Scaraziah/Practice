cd70e836f4789252ccf1325962507c7ee9cdabdd
Larry Hatcer <actswolf@gmail.com>
2022-09-15 17:43:15 -0500

    Set up next project and started problem 1.

---
+++ ProblemSolvingPart2/probSolvP2.jsx
@@ -0,0 +1,38 @@
+"use strict";
+
+// 1. Happy Numbers
+//     a.https://en.wikipedia.org/wiki/Happy_number
+//     b. A happy number is a number defined by the following process: starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1. An example of a happy number is 19
+//     c. Write a method that determines if a number is happy or sad
+
+
+
+
+
+
+
+
+
+
+// 2. Prime Numbers
+//     a. A prime number is a number that is only divisible by one and itself.
+//     b. Write a method that prints out all prime numbers between 1 and 100 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3. Fibonacci
+//     a. A series of numbers in which each number (Fibonacci number) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+//     b. Write a method that does the Fibonacci sequence starting at 1
+//     c. HARDER VERSION: Write a method that does the Fibonacci sequence starting at a number that a user inputs
